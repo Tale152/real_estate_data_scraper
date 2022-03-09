016e583d6fff3c2143b3b483efbfaa4fb3b95ad9
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-09 17:34:23 +0000

    working example using htmlcleaner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,36 @@
+import org.htmlcleaner.{CleanerProperties, HtmlCleaner}
 import scalaj.http.Http
+
+import java.net.URL
 
 object Main {
 
     def main(args: Array[String]): Unit = {
-      val result = Http("https://www.immobiliare.it/vendita-case/cesena/?criterio=dataModifica&ordine=desc")
+      /*val result = Http("https://www.immobiliare.it/vendita-case/cesena/?criterio=dataModifica&ordine=desc")
         .header("Content-Type", "text/html")
         .header("Charset", "UTF-8")
         .asString
-        .body
-      println(result)
+        .body*/
+      //println(result)
+
+      val prop = new CleanerProperties()
+      for (i <- 1 to 3){
+        //getting html from the url
+        val houseList = new HtmlCleaner(prop)
+          .clean(new URL("https://www.immobiliare.it/vendita-case/cesena/?criterio=dataModifica&ordine=desc&pag=" + i))
+          .findElementByName("body", false)
+          .findElementByAttValue("class", "nd-list in-realEstateResults", true, true)
+        if(houseList != null){
+          val houseBodies = houseList
+            .getElementsByName("a", true)
+            .map(e => new HtmlCleaner(prop)
+              .clean(new URL(e.getAttributeByName("href")))
+              .findElementByName("body", false)
+            )
+          houseBodies.foreach(h =>
+            println(h.findElementByAttValue("class", "im-titleBlock__title", true, true).getText.toString)
+          )
+        }
+      }
     }
 }
