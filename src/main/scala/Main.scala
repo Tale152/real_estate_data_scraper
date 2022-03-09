1266bc62de621d35339e65dcee708aeb99237e03
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-09 15:37:58 +0000

    working get request returning html as string

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,13 @@
+import scalaj.http.Http
+
 object Main {
 
     def main(args: Array[String]): Unit = {
-      println("Hello, world!")
+      val result = Http("https://www.immobiliare.it/vendita-case/cesena/?criterio=dataModifica&ordine=desc")
+        .header("Content-Type", "text/html")
+        .header("Charset", "UTF-8")
+        .asString
+        .body
+      println(result)
     }
 }
