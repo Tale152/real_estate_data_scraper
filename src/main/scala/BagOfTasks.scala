c23c6163f571c39989ebd512b721569b95ad93cf
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-10 16:50:23 +0000

    added private functions to simplify createBagOfTasks

--- src/main/scala/BagOfTasks.scala
+++ src/main/scala/BagOfTasks.scala
@@ -9,20 +9,25 @@ object BagOfTasks{
     val bagOfTasks = new util.HashSet[Task]()
     for (i <- 1 to 2){
       val houseList = new HtmlCleaner()
-        .clean(new URL("https://www.immobiliare.it/vendita-case/" + city + "/?criterio=dataModifica&ordine=desc&pag=" + i))
+        .clean(createHouseListUrl(city, i))
         .findElementByName("body", false)
         .findElementByAttValue("class", "nd-list in-realEstateResults", true, true)
       if(houseList != null){
         houseList
           .getElementsByName("a", true)
-          .map(e => e.getAttributeByName("href")
-            .replace("https://www.immobiliare.it/annunci/", "")
-            .replace("/", "")
-            .toLong
-          )
+          .map(e => extractIdFromHouseUrl(e.getAttributeByName("href")))
           .foreach(id => bagOfTasks.add(CompleteScrapingTask(id)))
       }
     }
     bagOfTasks
   }
+
+  private def createHouseListUrl(city: String, i: Int): URL =
+    new URL("https://www.immobiliare.it/vendita-case/" + city + "/?criterio=dataModifica&ordine=desc&pag=" + i)
+
+  private def extractIdFromHouseUrl(url: String): Long = url
+    .replace("https://www.immobiliare.it/annunci/", "")
+    .replace("/", "")
+    .toLong
+
 }
