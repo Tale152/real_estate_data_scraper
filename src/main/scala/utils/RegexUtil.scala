c0f972b9f7474c238b24ab4568bc0eaa8c5cc101
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-14 16:07:47 +0000

    added type to date to remove warning

--- src/main/scala/utils/RegexUtil.scala
+++ src/main/scala/utils/RegexUtil.scala
@@ -4,5 +4,5 @@ object RegexUtil {
   val day = "(([0-2][0-9])|(3[0-1]))"
   val month = "((0[0-9])|(1[0-2]))"
   val year = "((19[0-9][0-9])|(20[0-9][0-9]))"
-  val date = day + "/" + month + "/" + year
+  val date: String = day + "/" + month + "/" + year
 }
