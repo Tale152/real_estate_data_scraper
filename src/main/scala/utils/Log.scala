4f687c164dbb7a05c630a7d87cf9113bd612ccf3
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-03-16 12:18:13 +0000

    Added doc to Log

--- src/main/scala/utils/Log.scala
+++ src/main/scala/utils/Log.scala
@@ -1,5 +1,8 @@
 package utils
 
+/**
+ * Log with a tag used by [[scraping.ScrapingExecutors]] to print information about the status of the scraping.
+ */
 object Log {
 
   def log(str: String): Unit = println("[SCRAPER] " + str)
