074756c53937fb999848cd0cc404abac365be3a6
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-03-16 12:18:53 +0000

    Added doc to Sources

--- src/main/scala/utils/Sources.scala
+++ src/main/scala/utils/Sources.scala
@@ -1,6 +1,8 @@
 package utils
 
+/**
+ * Contains all the possible data sources used to scrape data from.
+ */
 object Sources {
   val IMMOBILIARE_IT = "immobiliare.it"
-  val CASA_IT = "casa.it"
 }
