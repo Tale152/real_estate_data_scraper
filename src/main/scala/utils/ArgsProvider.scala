775ddc2dcdef02a1055dde03f3b2dbd98f26528a
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-03-16 16:07:01 +0000

    Added doc to ArgsProvider

--- src/main/scala/utils/ArgsProvider.scala
+++ src/main/scala/utils/ArgsProvider.scala
@@ -1,29 +1,55 @@
 package utils
 
+import utils.ArgsProviderUtil.{Flags, Regex}
+
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-private object Flags{
-  val SOURCE: String = "-s"
-  val THREADS: String = "-t"
-  val STARTING_DATE: String = "-d"
+/**
+ * Utility for [[ArgsProvider]], containing Flags and Regex only used in it.
+ */
+private object ArgsProviderUtil {
+  /**
+   * Flags that the [[ArgsProvider]] can accept.
+   */
+  object Flags {
+    val SOURCE: String = "-s"
+    val THREADS: String = "-t"
+    val STARTING_DATE: String = "-d"
+  }
+
+  /**
+   * Regex used in the [[ArgsProvider]] to check if the arguments are correct.
+   */
+  object Regex {
+    val SOURCE: String = Flags.SOURCE + "=(?s)(.*)"
+    val THREADS: String = Flags.THREADS + "=[0-9]*"
+    val STARTING_DATE: String = Flags.STARTING_DATE + "=" + RegexUtil.date
+  }
 }
 
-private object Regex{
-  val SOURCE: String = Flags.SOURCE + "=(?s)(.*)"
-  val THREADS: String = Flags.THREADS + "=[0-9]*"
-  val STARTING_DATE: String = Flags.STARTING_DATE + "=" + RegexUtil.date
-}
-
+/**
+ * Case class that retrieve and check the arguments in input.
+ * @param args the arguments in input.
+ */
 case class ArgsProvider(private val args: Array[String]) {
 
+  /**
+   * @return the source argument.
+   */
   def source: String = getArg(Regex.SOURCE, Flags.SOURCE, "Source (-s) needs to be specified").toLowerCase
 
+  /**
+   * @return the LocalDate argument.
+   */
   def startingDate: LocalDate = {
     val d = getArg(Regex.STARTING_DATE, Flags.STARTING_DATE, "Starting date (-d) needs to be specified (dd/mm/yyyy)")
     parseDate(d)
   }
 
+  /**
+   * @return the number of threads argument.g
+   */
   def threads: Int = {
     val t = getArg(Regex.THREADS, Flags.THREADS, "Threads number (-t) to be specified (integer)").toLowerCase
     if(!t.forall(c => c.isDigit)) throw new IllegalArgumentException("The provided argument -t is not an integer number")
