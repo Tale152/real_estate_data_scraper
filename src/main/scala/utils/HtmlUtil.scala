b3c15a1e6c26fcbe67cae54a70ea4f7d52610f2e
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-03-16 13:13:13 +0000

    Added doc to HtmlUtil

--- src/main/scala/utils/HtmlUtil.scala
+++ src/main/scala/utils/HtmlUtil.scala
@@ -2,8 +2,16 @@ package utils
 
 import scalaj.http.Http
 
+/**
+ * Utility methods to handle Http calls.
+ */
 object HtmlUtil {
 
+  /**
+   * Get the html body found by calling a specific url.
+   * @param url a string that is the url of the html needed.
+   * @return a string which is the body of the html found by the Http call to the specified url.
+   */
   def getHtmlString(url: String): String = Http(url)
     .header("Content-Type", "text/html")
     .header("Charset", "UTF-8")
