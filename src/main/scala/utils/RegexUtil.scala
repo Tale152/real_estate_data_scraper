65c8e888c8d8107660b92ef70a954f6f4bace600
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-03-16 12:18:34 +0000

    Added doc to RegexUtil

--- src/main/scala/utils/RegexUtil.scala
+++ src/main/scala/utils/RegexUtil.scala
@@ -1,5 +1,8 @@
 package utils
 
+/**
+ * Contains the generic regexes that are not specific of any data source.
+ */
 object RegexUtil {
   val day = "(([0-2][0-9])|(3[0-1]))"
   val month = "((0[0-9])|(1[0-2]))"
